05a4ffea8e101ce9cb1132da0aeb0afaae67f03e
rasool-Max <rasool@aadvisolutions.com>
2024-07-11 11:05:54 +0530

    registration backend

--- src/api/routers/client.route.ts
+++ src/api/routers/client.route.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { ClientController } from '../controllers/client.controller';
 
-var clientRouter = express.Router();
+const clientRouter = express.Router();
 
 const clientController = new ClientController();
 
 console.info("inside clientcontroller");
-clientRouter.get('/get',clientController.getClients);
+clientRouter.get('/GetClients',clientController.getClients);
 clientRouter.post('/register',clientController.createClient);
 
 export default clientRouter;
