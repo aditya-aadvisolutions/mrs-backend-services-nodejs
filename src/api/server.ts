c40d9bac8a328ac073d7c08c4766bbbd9bc26b91
Aditya Reddy <aditya@aadvisolutions.com>
2024-07-19 01:07:15 +0530

    cors

--- src/api/server.ts
+++ src/api/server.ts
@@ -34,11 +34,17 @@ app.use(upload.none());
 // app.use(cors());
 // app.options()
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+const corsOptions = {
+    origin: '*', //'http://localhost:5173/',//(https://your-client-app.com)
+    optionsSuccessStatus: 200,
+  };
+ 
+  app.use(cors(corsOptions));
 
 app.use('/api/Job', jobrouter)
 app.use('/api/s3', multipartrouter);
