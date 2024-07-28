40d5d4d5d2e3747af5afe3bec878553c5f37fdb9
Aditya Reddy <aditya@aadvisolutions.com>
2024-07-28 12:00:24 +0530

    testing1

--- src/api/configuration/auth_config.ts
+++ src/api/configuration/auth_config.ts
@@ -7,7 +7,6 @@ const { NODE_ENV, PORT, SECRET_KEY, SECRET_IV, ECNRYPTION_METHOD } = process.env
 export default {
   env: NODE_ENV,
   port: PORT || 3000,
-  
   secret_key: SECRET_KEY,
   secret_iv: SECRET_IV,
   ecnryption_method: ECNRYPTION_METHOD,
