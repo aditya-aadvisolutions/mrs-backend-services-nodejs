2d2772295915e687bcf648bf131437bc8d48286c
SravaniBandirajula123 <sravani@aadvisolutions.com>
2024-08-06 19:33:34 +0530

    dashboard cards

--- src/api/services/dashboard.service.ts
+++ src/api/services/dashboard.service.ts
@@ -4,38 +4,41 @@ import MrsDatabase from "../../infra/database/mrs_db_connection";
 import { Dashboard } from '../models/dashboard';
 import { QueryTypes } from 'sequelize';
 
-export class DashboardServices{
-    async dashboard (username: string): Promise<Dashboard<any> | null>{
-       try {const replacements ={
-            LoginName: username
-        }
 
-        const sqlQuery = `exec USP_GetClientJobCounts
+
+export class DashboardServices {
+    async dashboard(username: string): Promise<Dashboard<any> | null> {
+        try {
+            const replacements = {
+                LoginName: username
+            }
+
+            const sqlQuery = `exec USP_GetClientJobCounts
         @LoginName = :LoginName`;
 
-        const [result] = await MrsDatabase.query(sqlQuery, {
-            replacements: replacements,
-            type: QueryTypes.RAW
-        });
-        const record=result[0];
-        if(record){
-            const count: Dashboard<any> ={
-                PendingJobsCount: record.PendingJobsCount,
-                CompletedJobsCount: record.CompletedJobsCount,
-                ClientsCount: record.ClientCount,
-                EmployeesCount: record.EmployeesCount,
-
+            const [result] = await MrsDatabase.query(sqlQuery, {
+                replacements: replacements,
+                type: QueryTypes.RAW
+            });
+            const record = result[0];
+            if (record) {
+                const count: Dashboard<any> = {
+                    PendingJobsCount: record.PendingJobsCount,
+                    CompletedJobsCount: record.CompletedJobsCount,
+                    ClientsCount: record.ClientCount,
+                    EmployeesCount: record.EmployeesCount,
+                    InProgressJobsCount: record.InProgressJobsCount,
+                    VoidJobsCount: record.VoidJobsCount,
+                    DuplicateJobsCount: record.DuplicateJobsCount,
+                    DownloadedJobsCount: record.DownloadedJobsCount,
+                }
+                return count;
+            } else {
+                console.log("No records found");
             }
-            return count;
-
-        }else 
-        {
-            console.log("No records Found")
+        } catch (error) {
+            throw new Error(error.message);
         }
-
-        
-}catch(error){
-    throw new Error (error.message)
-}
+        return null;
     }
 }
